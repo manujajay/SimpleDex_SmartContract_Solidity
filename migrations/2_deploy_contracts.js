51428c9f4ff98ed5fa2ab42ae164c6ae544e0946
manujajay <manujajayawardanais@gmail.com>
2023-11-12 00:06:15 +0000

    add latest files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const SimpleDEX = artifacts.require("SimpleDEX");
+const MockToken = artifacts.require("MockToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(SimpleDEX);
+module.exports = async function (deployer) {
+  await deployer.deploy(MockToken, "MockToken1", "MTK1", 18, 1000000);
+  const token1 = await MockToken.deployed();
+
+  await deployer.deploy(MockToken, "MockToken2", "MTK2", 18, 1000000);
+  const token2 = await MockToken.deployed();
+
+  deployer.deploy(SimpleDEX, token1.address, token2.address);
 };
